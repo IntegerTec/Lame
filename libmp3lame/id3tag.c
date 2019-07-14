6401f7f170a139d15b13b94ae53c2758aef199b0
LuciusZhang <z360394624@gmail.com>
2019-07-14 20:51:11 +0800

    modify for android NDK

--- libmp3lame/id3tag.c
+++ libmp3lame/id3tag.c
@@ -42,13 +42,13 @@
 # include <ctype.h>
 #else
 # ifndef HAVE_STRCHR
-#  define strchr index
-#  define strrchr rindex
+// #  define strchr index
+// #  define strrchr rindex
 # endif
 char   *strchr(), *strrchr();
-# ifndef HAVE_MEMCPY
-#  define memcpy(d, s, n) bcopy ((s), (d), (n))
-# endif
+// # ifndef HAVE_MEMCPY
+// #  define memcpy(d, s, n) bcopy ((s), (d), (n))
+// # endif
 #endif
 
 
